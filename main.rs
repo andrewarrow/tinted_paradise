58b1192dcc79631b2a0680b4635fbe7d095eeea2
Andrew Arrow <andrew@0x7a69.net>
2016-08-10 23:03:41 +0000

    progress

--- main.rs
+++ main.rs
@@ -1,9 +1,21 @@
 use std::net::{TcpListener,TcpStream};
+use std::io::{BufRead,BufReader,Read};
 use std::thread;
 
 fn handle_client(stream: TcpStream) {
   let addr = stream.peer_addr().unwrap();
   println!("Got connection from {}", addr);
+
+  //let mut writer = stream.try_clone().unwrap();
+  let mut reader = BufReader::new(stream);
+  let mut data: Vec<u8> = Vec::new();
+
+  let mut file_name_buf = String::new();
+  reader.read_line(&mut file_name_buf).unwrap();
+
+  reader.read_to_end(&mut data).unwrap();
+
+  println!("gots the datas...{}",data.len());
 }
 
 fn main() { 
