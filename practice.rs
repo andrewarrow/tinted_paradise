8fb754433d06a89965306ce74f9bb223c03afbba
Andrew Arrow <andrew@0x7a69.net>
2016-08-19 09:32:28 +0000

    cool

--- practice.rs
+++ practice.rs
@@ -2,7 +2,12 @@ extern crate rand;
 use rand::Rng;
 
 fn main() {
-  // a number from [-40.0, 13000.0)
-  let num = rand::thread_rng().gen_range(0, 255);
-  println!("{}", num);
+  let mut vec = vec![1];
+  for _ in 0..100 {
+    let num = rand::thread_rng().gen_range(0, 255);
+    vec.push(num)
+  }
+  for x in &vec {
+    println!("{}", x);
+  }
 }
