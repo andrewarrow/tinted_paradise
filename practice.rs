beca125e01b87ef4563f10b42ecdce2a3b26b1a9
Andrew Arrow <andrew@0x7a69.net>
2016-08-19 13:06:25 +0000

    great

--- practice.rs
+++ practice.rs
@@ -1,13 +1,16 @@
 extern crate rand;
 use rand::Rng;
+use std::collections::HashMap;
 
 fn main() {
+  let mut hash = HashMap::new();
   let mut vec = vec![1];
   for _ in 0..100 {
     let num = rand::thread_rng().gen_range(0, 255);
     vec.push(num)
   }
-  for x in &vec {
-    println!("{}", x);
-  }
+  println!("{}", vec.len());
+
+  hash.insert("Daniel", vec);
+  println!("{:?}", hash);
 }
