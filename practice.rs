eaed5207b4d6d08d48525066e172d5d064a31917
Andrew Arrow <andrew@0x7a69.net>
2016-08-19 18:54:52 +0000

    good

--- practice.rs
+++ practice.rs
@@ -3,7 +3,7 @@ use rand::Rng;
 use std::collections::HashMap;
 use std::{thread, time};
 
-fn count(m: &mut HashMap<String, Vec<i32>>) {
+fn delete_from(m: &mut HashMap<String, Vec<i32>>) {
   loop {
     println!("{}", m.len());
     let mut i = 1;
@@ -20,26 +20,32 @@ fn count(m: &mut HashMap<String, Vec<i32>>) {
   }
 }
 
-fn main() {
-  let mut hash: HashMap<String, Vec<i32>> = HashMap::new();
+fn ran_vector() -> Vec<i32> {
   let mut vec: Vec<i32> = vec![1];
   for _ in 0..100 {
     let num: i32 = rand::thread_rng().gen_range(0, 255);
     vec.push(num)
   }
-  println!("{}", vec.len());
+  vec
+}
+
+fn ran_filename() -> String {
+  let mut filename = String::new(); 
+  for _ in 0..35 {
+    let num = rand::thread_rng().gen_range(65, 105);
+    let c = std::char::from_u32(num).unwrap();
+    filename.push(c)
+  }
+  filename
+}
+
+fn main() {
+  let mut hash: HashMap<String, Vec<i32>> = HashMap::new();
     
   for _ in 0..35 {
-    let mut filename = String::new(); 
-    for _ in 0..35 {
-      let num = rand::thread_rng().gen_range(65, 105);
-      let c = std::char::from_u32(num).unwrap();
-      filename.push(c)
-    }
-    hash.insert(filename, vec.clone());
+    hash.insert(ran_filename(), ran_vector());
   }
 
-  //println!("{:?}", hash);
-  let t1 = thread::spawn(move || { count(&mut hash); });
+  let t1 = thread::spawn(move || { delete_from(&mut hash); });
   let _ = t1.join();
 }
