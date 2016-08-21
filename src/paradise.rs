c64343c3bb861c92bdbd92488cfb0d4a0156052b
Andrew Arrow <andrew@0x7a69.net>
2016-08-21 17:52:56 +0000

    wow

--- src/paradise.rs
+++ src/paradise.rs
@@ -1,32 +1,11 @@
 use std::net::{TcpListener,TcpStream};
 use std::thread;
-use std::io::Write;
 use std::str;
 use std::io::BufReader;
 use std::io::BufRead;
 
 pub use self::starter::Paradise;
 mod starter;
-
-fn handle_client(mut stream: TcpStream) {
-  println!("{:?}", stream);
-  let addr = stream.peer_addr().unwrap();
-  println!("Got connection from {}", addr);
-
-  match stream.write(b"220 Welcome to Tinted Paradise\r\n") {
-            Err(_) => {},
-            Ok(_) => {},
-        }
-
-  let mut br = BufReader::new(&stream);
-
-  loop {
-
-    let mut buffer = String::new();
-    let _ = br.read_line(&mut buffer);
-    println!("{:?}", buffer);
-  }
-}
 
 fn main() { 
   let listener = TcpListener::bind("127.0.0.1:2121").unwrap();
@@ -35,8 +14,12 @@ fn main() {
     match stream {
       Err(e) => { println!("failed: {}", e) }
       Ok(stream) => {
-        let p = Paradise::new(stream);
+        let addr = stream.peer_addr().unwrap();
+        println!("Got connection from {}", addr);
+
+        let mut p = Paradise::new(stream);
         println!("{:?}", p);
+        p.start();
       }
     }
   }
--- src/starter.rs
+++ src/starter.rs
@@ -1,5 +1,6 @@
 
 use std::net::{TcpStream};
+use std::io::Write;
 
 #[derive(Debug)]
 pub struct Paradise {
@@ -7,7 +8,30 @@ pub struct Paradise {
 }
 
 impl Paradise {
-  pub fn new(mut stream: TcpStream) -> Paradise {
+  pub fn new(stream: TcpStream) -> Paradise {
     Paradise {cstream: stream}
   }
+
+  pub fn start(&mut self) {
+    self.writeMessage(220, "Welcome to Paradise");
+  }
+
+  pub fn writeMessage(&mut self, code: i32, message: &str) {
+    let buffer = String::new();
+    self.cstream.write(b"220 Welcome to Tinted Paradise\r\n");
+  }
 }
+
+
+/*
+fn handle_client(mut stream: TcpStream) {
+  let mut br = BufReader::new(&stream);
+
+  loop {
+
+    let mut buffer = String::new();
+    let _ = br.read_line(&mut buffer);
+    println!("{:?}", buffer);
+  }
+}
+*/
