5c30cc280e056a0ab4050441d4f03f3f778c408a
Andrew Arrow <andrew@0x7a69.net>
2016-08-21 23:26:36 +0000

    COMMAND_MAP

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,14 @@ extern crate paradise;
 
 use std::net::TcpListener;
 use std::thread;
+use std::collections::HashMap;
 use paradise::server::Paradise;
 
 fn main() { 
+  let mut command_map: HashMap<String, String> = HashMap::new();
+  command_map.insert("USER".to_string(), "1".to_string());
+  println!("{:?}", command_map);
+
   let listener = TcpListener::bind("127.0.0.1:2121").unwrap();
   println!("listening started, ready to accept");
   for stream in listener.incoming() {
